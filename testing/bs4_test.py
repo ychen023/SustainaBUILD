e837964ae6b6a943bf02c1103345ef454d5adf50
choijos <choijos@uw.edu>
2022-02-18 20:42:33 -0800

    more fleshed out vegware scraping

--- testing/bs4_test.py
+++ testing/bs4_test.py
@@ -2,36 +2,74 @@ from html.parser import HTMLParser
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://www.vegwareus.com/us/catalogue/soup_containers/"
-headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Mobile Safari/537.36'}
-result = requests.get(url, headers=headers)
-doc = BeautifulSoup(result.text, "html.parser")
+headers = {'User-Agent': """ input user agent here """}
 
-prices = doc.find_all(class_="subCat")
+# returns a dictionary of vegware product categories as keys and
+#   their href urls as values
+def list_fill_vegware_categories():
+  result = requests.get("https://www.vegwareus.com/us/catalogue/", headers=headers)
+  doc = BeautifulSoup(result.text, "html.parser")
+  urls = {}
 
-# going through all sub categories
-for i in range(len(prices)):
-  print("-----------------")
-  curr = prices[i]
-  print(curr.find(class_="subCat__header-title").string)
-  curr_prods = curr.find_all(class_="product")
+  # grabbing all the category tags
+  cats = doc.find_all(class_="catBox")
 
-  # going through each prod in each sub category
-  for j in range(len(curr_prods)):
-    curr_prod = curr_prods[j]
-    curr_prod_name = curr_prod.find(class_="product__title").find("a").string
-    curr_prod_stock = curr_prod.find(class_="product__stock-message")
+  for cat_box in cats:
+    cat_name = cat_box.find(class_="catBox__title").text
+    # Categories we don't want to include
+    if cat_name == "Home essentials" or cat_name == "Delivery":
+      continue
 
-    if curr_prod_stock.find("span") is None:
-      curr_prod_stock = 'Out of stock'
+    cat_url = "https://www.vegwareus.com" + cat_box["href"]
+    urls[cat_name] = cat_url
 
-    else:
-      curr_prod_stock = (curr_prod_stock.find("span").text)[1:]
+  return urls
 
-    print("  " + curr_prod_name + " - (" + curr_prod_stock + ")")
-    curr_prod_prices = curr_prod.find_all(class_="pricing__single")
 
-    # going through all price options & breakdowns for each prod
-    for k in range(len(curr_prod_prices)):
-      single_pricing = curr_prod_prices[k].find(class_="pricing__price").string + curr_prod_prices[k].find(class_="pricing__type").string
-      print("    " + single_pricing)
+# going through each category
+def proc_vegware_categories(cat_name, cat_url):
+  result = requests.get(cat_url, headers=headers)
+  page = BeautifulSoup(result.text, "html.parser")
+
+  print("------- \n")
+  print(cat_name)
+
+  # going through sub categories
+  for sub_cat in page.find_all(class_="subCat"):
+    print()
+    sub_cat_name = sub_cat.find(class_="subCat__header-title").text
+    print(" ", sub_cat_name, ":")
+
+    # going through each product in each sub category
+    for prod in sub_cat.find_all(class_="product"):
+      prod_header = prod.find(class_="product__title").find("a")
+      prod_name = prod_header.text
+      prod_url = "https://www.vegwareus.com" + prod_header["href"]
+
+      prod_stock_status = prod.find(class_="product__stock-message")
+      if prod_stock_status.find("span") is None:
+        # out of stock
+        prod_stock_status = "Out of stock"
+      
+      else:
+        prod_stock_status = (prod_stock_status.find("span").text)[1:] # removing the extra space
+
+      print("    " + prod_name + " (" + prod_stock_status + ") -> " + prod_url)
+
+      # going through each pricing option/breakdown
+      for price in prod.find_all(class_="pricing__single"):
+        dollar_amount = price.find(class_="pricing__price").text
+        breakdown = price.find(class_="pricing__type").text
+
+        print("     ", dollar_amount, breakdown)
+
+
+#
+def main():
+  urls = list_fill_vegware_categories()
+
+  for name, url in urls.items():
+    proc_vegware_categories(name, url)
+    print()
+
+main()
